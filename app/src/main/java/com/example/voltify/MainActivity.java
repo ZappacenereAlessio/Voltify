737567f1a84c560b404c046ea0fa47b3a1d0b08c
MBartoccioni <matteo.barto57@gmail.com>
2022-01-18 09:49:16 +0100

    +Spinner

--- app/src/main/java/com/example/voltify/MainActivity.java
+++ app/src/main/java/com/example/voltify/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+        String[] elencoGeneri = {"pop", "Kpop", "Jpop"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         EditText durata=(EditText)findViewById(R.id.editText3);
 
         Spinner genere=(Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> spGenere = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, elencoGeneri);
+        genere.setAdapter(spGenere);
 
         GestoreBrani gestoreBrani = new GestoreBrani();
 
@@ -31,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 gestoreBrani.aggBrano(titolo.getText().toString(), autore.getText().toString(), genere.getSelectedItem().toString(), Integer.getInteger(durata.getText().toString()));
-
+               /* String testo = genere.getSelectedItem().toString();
+                Toast.makeText(getApplicationContext(), testo,Toast.LENGTH_LONG).show();*/
             }
         });
 
